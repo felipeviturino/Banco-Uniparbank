6832c95bad59970cef7c753f0d1c189f5ef441c6
felipeviturino <felipeviturino2008@gmail.com>
2026-05-18 20:26:41 -0300

    Banco Unipar

---
+++ script.js
@@ -0,0 +1,70 @@
+function calcularBanco(){
+    let saldo = Number(prompt("Digite o saldo inicial"))
+    let deposito = Number(prompt("Digite o valor do deposito"))
+    let saque = Number(prompt("Digite o valor do saque"))
+    let taxa = Number(prompt("Digite a taxa de juros"))
+    let anos = Number(prompt("Digite os anos do investimento"))
+    
+
+    //Saldo total da conta
+    saldo = saldo + deposito
+
+    //saque
+    saldo = saldo - saque
+
+    //rendimento
+    let rendimento = saldo * (taxa / 100)
+
+    //meta de saldo
+    let metadeSaldo = saldo / 2
+
+    //resto da divisão
+    let saldo_par = saldo % 2 ==0
+
+    //Juros compostos
+    let jurosCompostos =
+    saldo * (1 + taxa / 100) ** anos
+
+    /*saldo +=50
+
+    saldo -= 20
+
+    saldo *= 2
+
+    saldo /= 2
+
+    saldo **= 2*/
+
+    document.getElementById("resultado").innerHTML = `
+
+    <strong>Saldo:</strong>
+    R$ ${saldo.toFixed(2)}
+    
+    <strong>Rendimento:</strong>
+    R$ ${rendimento.toFixed(2)} <br>
+    
+    <strong>Metade do saldo:</strong>
+    R$ ${metadeSaldo.toFixed(2)} <br>
+
+    <strong>Saldo é:</strong>
+    ${saldo_par ? 'número par' : 'número ímpar'} <br>
+
+    <strong>Juros compostos:</strong>
+    R$ ${jurosCompostos.toFixed(2)} <br>
+
+    `
+    /*
+    if(teste){
+    //executa um bloco de código
+    }else{
+        //executa outro
+    }
+        */
+       //teste ? resultado se for verdade: se for falso
+       //-> ternario no JS
+}
+
+function limparcaixa(){
+    document.getElementById('resultado').innerHTML="";
+}
+
